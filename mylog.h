9ef8f46136e7dc047bc1ecc40c49df3f20243ab0
panaka_13 <nhatkhang589@gmail.com>
2020-06-24 01:04:23 -0500

    initial commit and setup

---
+++ mylog.h
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "configure.h"
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+#include "configure.h"
+
+int main() {
+  std::cout << "mylog VERSION " << VERSION << std::endl;
+}
